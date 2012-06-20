380e7db25fe9d883bfd6af9189db6d0385eb85ed
Szczepan Faber <szczepiq@gmail.com>
2012-06-20 20:25:17 +0200

    Tweaked the sample to be demoable from scratch. Added some documentation

--- test-listener/src/test/java/org/gradle/tests0/Test15.java
+++ test-listener/src/test/java/org/gradle/tests0/Test15.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class Test15 {
     @Test
     public void myTest() throws Exception {
-        Thread.sleep(5);
+        Thread.sleep(200);
         if (false) {
            org.junit.Assert.assertTrue(false);
         }
